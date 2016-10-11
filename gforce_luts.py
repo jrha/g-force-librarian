6b469f13b7678545bd85adfe7cbfa614963f71e7
James Adams <james@bluezen.co.uk>
2016-10-12 00:55:11 +0100

    Add lookup tables

---
+++ gforce_luts.py
@@ -0,0 +1,59 @@
+EFFECT_TYPES = [
+    '',
+    'Pass Thru',
+    '? 02 ?',
+    '? 03 ?',
+    'Compressor',
+    'Filter',
+    'Pan + Tremolo',
+    'Pitch Shift',
+    'Delay',
+    'Drive',
+    'Chorus',
+    'Reverb',
+]
+
+EFFECT_STATES = [
+    'Not Present',
+    'Active',
+    '? Muted ?',
+    'Bypassed',
+]
+
+COMPRESSOR_RATIOS = [
+    'Off',
+    '1.12:1',
+    '1.25:1',
+    '1.40:1',
+    '1.60:1',
+    '1.80:1',
+    '2.0:1',
+    '2.5:1',
+    '3.2:1',
+    '4.0:1',
+    '5.6:1',
+    '8.0:1',
+    '16:1',
+    '32:1',
+    '64:1',
+    'Infinite',
+]
+
+COMPRESSOR_KNEE_MODES = [
+    'Soft',
+    'Hard',
+]
+
+GLOBAL_MUTE_MODES = [
+    'Mix 0%',
+    'FX Out',
+    'Output',
+]
+
+FILTER_ALGORITHMS = [
+    'Parametric',
+    'Resonance',
+    'Wah-wah',
+    'Formant',
+    'Phaser',
+]
---
+++ tc_globals.py
@@ -0,0 +1,16 @@
+TC_MANUFACTURER_ID = (0x00,0x20,0x1f)
+
+TC_MODELS = {
+    0x10 : 'Header',
+    0x11 : 'G-Force',
+    0x40 : 'FireworX',
+    0x42 : 'M3000',
+    0x44 : 'M-One',
+    0x45 : 'D-Two',
+    0x47 : 'Triple C',
+    0x63 : 'Nova System',
+}
+
+TC_MODELS_SUPPORTED = [
+    0x11,
+]
